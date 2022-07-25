bbc736b6d163a4d3f851b36222269b96a9385938
Savics407 <chibuvic135@gmail.com>
2022-07-25 01:57:34 +0100

    Site is ready for initial testing

--- src/util/PrivateRoutes.js
+++ src/util/PrivateRoutes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 function PrivateRoutes() {
-  let auth = localStorage.getItem("user-id");
+  let auth = localStorage.getItem("user-token");
 
   return !!auth ? <Outlet /> : <Navigate to="/login" />;
 }
